5fa5623495ff6a4f067176d16787e08aaac23965
Musickun5659 <103948827+Musickun5659@users.noreply.github.com>
2022-06-17 09:15:10 -0400

    rc car

--- main.ts
+++ main.ts
@@ -1,20 +1,43 @@
-let steering_angle_P2 = 0
-let steering_angle_P4 = 0
-input.onButtonPressed(Button.A, function () {
+enum RadioMessage {
+    message1 = 49434,
+    reverse = 52434,
+    accelerate = 42979,
+    lights = 8859,
+    left = 14947,
+    right = 32391
+}
+radio.onReceivedMessage(RadioMessage.accelerate, function () {
     servos.P0.run(100)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
+    radio.sendMessage(RadioMessage.accelerate)
+})
+radio.onReceivedMessage(RadioMessage.left, function () {
+    servos.P1.setAngle(30)
+})
+radio.onReceivedMessage(RadioMessage.lights, function () {
     pins.digitalWritePin(DigitalPin.P3, 1)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendMessage(RadioMessage.lights)
+})
 input.onButtonPressed(Button.B, function () {
+    radio.sendMessage(RadioMessage.reverse)
+})
+radio.onReceivedMessage(RadioMessage.reverse, function () {
     servos.P0.run(-75)
 })
+radio.onReceivedMessage(RadioMessage.right, function () {
+    servos.P1.setAngle(150)
+})
+let steering_angle_P4 = 0
+radio.setGroup(3)
+servos.P1.setAngle(90)
 basic.forever(function () {
-    steering_angle_P2 = pins.digitalReadPin(DigitalPin.P2)
     steering_angle_P4 = pins.digitalReadPin(DigitalPin.P4)
-    if (steering_angle_P2) {
-        servos.P2.setAngle(30)
-    } else if (steering_angle_P4) {
-        servos.P2.setAngle(150)
+    if (input.pinIsPressed(TouchPin.P2)) {
+        radio.sendMessage(RadioMessage.left)
+    } else if (steering_angle_P4 == 1) {
+        radio.sendMessage(RadioMessage.right)
     }
 })
